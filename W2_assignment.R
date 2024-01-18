eb179755f01b8856a1e880403befbec576cb3565
rensol1 <renata@cogeco.ca>
2024-01-18 14:12:12 -0500

    add plot

--- W2_assignment.R
+++ W2_assignment.R
@@ -4,7 +4,6 @@ green_space_data <- read_csv("Green_Space_Biodiversity_Partial.csv")
 problems(green_space_data)
 
 ## Convert to long format ----
-
 green_space_long <- green_space_data %>% 
   pivot_longer(
     cols = c(butterfly, bird),
@@ -23,5 +22,13 @@ greenspaceTable <- (green_space_long
 
 summary(greenspaceTable)
 
+## Visually check normality assumption ----
+lm_green_space<- lm(species.richness ~ animal, data = greenspaceTable)
+qqnorm(residuals(lm_green_space),main=""); qqline(residuals(lm_green_space)) 
+# The dots visually look close to the line, so we can probably assume normality
 
-saveRDS(green_space_long, file = "greenspaceTable.rds")
+# To double check, we could also conduct a Shapiro-Wilk normality test
+shapiro.test(residuals(lm_green_space))
+# The test is non-significant (p = 0.337), so we can assume normality 
+
+saveRDS(greenspaceTable, file = "greenspaceTable.rds")
