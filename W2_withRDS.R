1fc6aacae2725167f2217bc1cac95215222e42d2
rensol1 <renata@cogeco.ca>
2024-01-18 14:12:18 -0500

    remove plot

--- W2_withRDS.R
+++ W2_withRDS.R
@@ -1,15 +1,5 @@
 greenspace_data <- readRDS(file = "greenspaceTable.rds")
 
-## Plots to check for errors ----
-## Visually check normality assumption ----
-lm_green_space<- lm(species.richness ~ animal, data = greenspace_data)
-qqnorm(residuals(lm_green_space),main=""); qqline(residuals(lm_green_space)) 
-# The dots visually look close to the line, so we can probably assume normality
-
-# To double check, we could also conduct a Shapiro-Wilk normality test
-shapiro.test(residuals(lm_green_space))
-# The test is non-significant (p = 0.337), so we can assume normality 
-
 ## Visualize data with a boxplot ----
 library(ggplot2)
 green_space_boxplot <- (ggplot(greenspace_data, 
