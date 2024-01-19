64f9e5a77b7001e603d80995205ea815f6b02858
rensol1 <renata@cogeco.ca>
2024-01-19 12:35:23 -0500

    Add explanations

--- W2_assignment.R
+++ W2_assignment.R
@@ -1,6 +1,8 @@
+## Import data ----
 library(tidyverse)
 green_space_data <- read_csv("Green_Space_Biodiversity_Partial.csv")
 
+## Check for any obvious problems in the dataset ----
 problems(green_space_data)
 
 ## Convert to long format ----
@@ -16,8 +18,10 @@ greenspaceTable <- (green_space_long
                  %>% mutate(site=as.factor(site)) 
                  %>% mutate(animal=as.factor(animal))
 )
-
 summary(greenspaceTable)
+# Summary table looks correct
+# Each site has 2 data points (1 for bird and 1 for butterfly)
+# There are 15 bird and butterfly data points for 15 sites (A–O)
 
 ## Visually check normality assumption ----
 lm_green_space<- lm(species.richness ~ animal, data = greenspaceTable)
@@ -28,4 +32,5 @@ qqnorm(residuals(lm_green_space),main=""); qqline(residuals(lm_green_space))
 shapiro.test(residuals(lm_green_space))
 # The test is non-significant (p = 0.337), so we can assume normality 
 
+## Save object as RDS file ----
 saveRDS(greenspaceTable, file = "greenspaceTable.rds")
--- W2_withRDS.R
+++ W2_withRDS.R
@@ -1,3 +1,4 @@
+## Import RDS file ----
 greenspace_data <- readRDS(file = "greenspaceTable.rds")
 
 ## Visualize data with a boxplot ----
@@ -9,7 +10,7 @@ green_space_boxplot <- (ggplot(greenspace_data,
 )
 print(green_space_boxplot)
 # It seems like the bird and butterfly groups may have unequal variances
-# To double check, we could also conduct a Bartleet test of homogeneity of variances 
+# To double check, we could also conduct a Bartlett test of homogeneity of variances 
 bartlett.test(species.richness ~ animal, data = greenspace_data)
 
 # The test is significant (p = 0.019), suggesting that the groups have unequal variances 
