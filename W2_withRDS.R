dc4953cba8a1a811046651202a98fa177a42488f
rensol1 <renata@cogeco.ca>
2024-01-19 12:39:21 -0500

    spacing

--- W2_withRDS.R
+++ W2_withRDS.R
@@ -10,9 +10,9 @@ green_space_boxplot <- (ggplot(greenspace_data,
 )
 print(green_space_boxplot)
 # It seems like the bird and butterfly groups may have unequal variances
+
 # To double check, we could also conduct a Bartlett test of homogeneity of variances 
 bartlett.test(species.richness ~ animal, data = greenspace_data)
-
 # The test is significant (p = 0.019), suggesting that the groups have unequal variances 
 
 
