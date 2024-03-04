737a55b7216fdfc5030ee674fa49b778324a473b
rensol1 <renata@cogeco.ca>
2024-03-04 13:30:56 -0500

    actual final again

--- W7Assignment.R
+++ W7Assignment.R
@@ -1,23 +1,33 @@
 #W7 Assignment 
 library(MASS)
-
+library(tidyverse)
+library(emmeans)
+library(coefplot)
+install.packages("coefplot")
 #This RDS file is created by "W2_assignment.R" in my repo 
 greenspace_data <- readRDS(file = "greenspaceTable.rds")
 
-#Hypothesis: greenspace area size (in ha.) has an effect on species richness in that area
-greenspace_glm <- glm(species.richness~area.ha, data=greenspace_data, family="poisson" )
+greenspace_glm <- glm(species.richness~animal , data=greenspace_data, family="poisson" )
 #summary(greenspace_glm)
 
-plot(greenspace_glm)
-#This creates diagnostic plots
-#The residuals vs. fitted plot is the first we should examine because it allows us to look for patterns or trends in the residuals. I don't see any particular (obvious) trends. 
-#The Q-Q residuals plot is shown next, though really we should consider it last. Points near the line indicate residuals are normally distributed, whereas points far from the line indicate they may be non-normally distributed. The 3 highest points are the farthest away from the line, and may indicate non-normality at larger theoretical quantiles
-#The Scale-Location plot is used to visually examine whether there is heteroscedasticity, which is shown if the line deviates from horizontal. There is quite a large dip in the line between 2.2 and 2.3 of the predicted values. 
-#The Residuals vs Leverage shows points with high leverage; points outside the dotted lines have high leverage (influence on the model). There is a point near the bottom right corner that has high leverage. 
+
+#Diagnostic plots
+greenspace_sim <- simulateResiduals(fittedModel = greenspace_glm)
+plot(greenspace_sim)
+#The Q-Q residuals plot: Points near the line indicate residuals are normally distributed, whereas points far from the line indicate they may be non-normally distributed. This tells me there is a significant deviation, meaning the points are non-normally distributed
+#The Levene Test for homogeneity of variance is non-significant, meaning that the variance of the two groups is the same
 
 
-##I used this published data because I don't have other count or proportion data
-#I do have some ordinal data that I am currently collecting. I used it to discuss last week's assignment.
+#Inferential plot
+coefplot(greenspace_glm)
+#This visualizes the coefficients of the glm, as well as the confidence intervals. Essentially, it is a way to visually summarize the glm.
+#The intercept in this case is bird.
+#We can see that butterfly has less species richness than bird. 
+#However, I have not been using this data, so I am not sure if this difference is biologically meaningful.
+#See below for what I was trying to do, but could not figure out
+
+
+#I  have some ordinal data that I am currently collecting. I used it to discuss last week's assignment.
     #I have been treating this round of data collection as an exploration of my methods and analyses
 
 #I'm having trouble figuring out how to model it
@@ -37,4 +47,14 @@ flightscore_glm <- polr(Flight_Score ~ Group, data = flight_data_long, Hess=TRUE
 #summary(flightscore_glm)
 #This seems to work? However, I don't know what to do next 
 
+# flight_sim <- simulateResiduals(fittedModel = flightscore_glm)
+# plot(flight_sim)
+#This doesn't work 
 
+#plot(flightscore_glm)
+#This also doesn't work
+
+#I can create a coefficient plot
+coefplot(flightscore_glm)
+#But I'm not exactly sure how to interpret it (or if it's correct)
+#Sorry:(
